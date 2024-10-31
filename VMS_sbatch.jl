575ce9d987058bd6648e0dc9bacb40fcb89a97f5
Christian Aguirre <visdenchris@gmail.com>
2024-10-31 12:54:57 -0700

    termination criteria

--- VMS_sbatch.jl
+++ VMS_sbatch.jl
@@ -390,7 +390,7 @@ function model_setup(options::Dict,plot_dir::String,io)
         f_A = @sprintf("%.6g",Af/1e3)
 
         # Checking Termination Criteria, time is in Myr, amplitude is in meters
-        if time >= max_time || itime >= max_step || 0.10*(1/exp(1)) <= 1/exp(1) && (ice_shell_thickness[itime] - ice_shell_thickness[1]) > (options["hice"] * 0.10) && Af/Ai <= 1/exp(1)
+        if time >= max_time || itime >= max_step || (Af/Ai <= 1/exp(1) && (ice_shell_thickness[itime] - ice_shell_thickness[1]) > (options["hice"] * 0.10))
             terminate = true
             ### Final Plots ###
             get_plots_new(grid,Snew,Tnew,Xnew,"final",plot_dir)
